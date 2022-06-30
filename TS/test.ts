c77f10495bd793238301e4ab1fdbf49a0eff9ab1
张圣 <zhangsheng@fineyun.cn>
2022-06-30 13:18:26 +0800

    泛型

--- TS/test.ts
+++ TS/test.ts
@@ -1,25 +1,3 @@
-interface inter {
-    name: string
-    age: number
-}
-class Person implements inter {
-    name: string
-    age: number
-    constructor(name: string, age: number) {
-        this.name = name
-        this.age = age
-    }
-}
-const obj: inter = {
-    name: "string",
-    age: 123,
-}
-class Animal {
-    private _name = "123"
-    get name() {
-        return this._name
-    }
-    set name(value) {
-        this._name = value
-    }
-}
+let a: unknown = 0
+a ??= "123"
+console.log(a);
