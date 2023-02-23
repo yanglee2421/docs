5ea754489ffd6820a49fa69ce17f855e73fcfef8
李又土 <xtcff082421@gmail.com>
2023-02-23 22:37:53 +0800

    环境变量

--- src/node/nodejs/03_process.md
+++ src/node/nodejs/03_process.md
@@ -1,27 +1,39 @@
 # process
 
+## env
+
+用于获取当前运行环境的环境变量
+
+::: code-group
+
+```js
+process.env;
+```
+
+```powershell
+$Env:VAR_NAME#查看
+$Env:VAR_NAME="指定内容"#增/改
+$Env:VAR_NAME=""#删
+$Env:VAR_NAME+="附加内容"#附加
+```
+
+:::
+
 ## cwd
+
+返回当前工作目录，不同调试方式的工作目录有所不同
 
 ```js
 process.cwd();
 ```
 
-::: tip
-
-- 返回当前工作目录
-- 不同调试方式的工作目录有所不同
-
-:::
-
 ## exit
+
+结束当前进程
 
 ```js
 process.exit();
 ```
-
-::: tip
-结束当前进程
-:::
 
 ## nextTick
 
@@ -34,12 +46,12 @@ process.nextTick(() => {
 ::: warning
 
 - 根据模块化方式的不同，栈的执行顺序也不同
-- cjs 下
+- cjs 下：
   1. 执行栈
   2. tick 栈
   3. 微任务栈
   4. 宏任务栈
-- mjs 下
+- mjs 下：
   1. 执行栈
   2. 微任务栈
   3. tick 栈
--- test/demo.test.js
+++ test/demo.test.js
@@ -1,4 +1,20 @@
-const str = `
-<p><b> FEATURE:<br></b>- Made of ABS and stainless steel material. Non-toxic, tasteless and safe to use.<br>- Multifunctional. The blades allow flexibility to easily cut, slice, shred, chop fruits and vegetables in shapes and sizes. Such as a potato, you can make it into wavy or normal slices, strips or mash it easily. Meet your different needs.<br>- Saving time and effort. Ultra-sharp stainless steel blades can help you shorten prep time comparing with traditional blade slicing.<br>- The unique design prevents your hands from ever going near the built-in blades, making this mandoline the safest option for an expert slice.<br>- Wide Application. Designed to chop all kinds of food, such as onion, potato, mushroom, cucumber, carrot, tomato, ginger, garlic, cheese, etc.<br>- Easy to assemble and remove.<br>- Compact design for easy folding and easy storage.<br>- Excellent gift for your families, friends, etc.<br> <br><b>SPECIFICATION:<br></b>Type: Multifunctional Food Chopper<br>Color: Grey/ Orange/ Blue<br>Assembled Size: 13*23*34 cm<br>Material: ABS, Stainless steel<br>Thickness Adjustment: 0-8 level (8 mm-0.5 mm)<br>Suitable for: Vegetable, Fruit, Frozen Meat, etc<br>Weight: 0.65 kg<br> <br><b>PACKAGE INCLUDED:<br></b>1 X Multifunctional Food Chopper<br>1 X Cleaning Brush<br>1 X Blade<br>1 X User Manual<br> <br><b>NOTE:<br></b>Meat can only be cut into frozen meat .<br>Please allow 1-3cm error due to manual measurement.<br>If you have any quetestion, please feel free to contact us in time. We are always here and happy to provide assistance.<br></p>`;
+const str =
+  "Your input tokens are more than your left credit. Please upgrade your plan at htt-ps://warp-driven.com!";
 
-console.log(str.replace(/\<\/?\w+\>/g, ""));
+function handleLink(str) {
+  if (typeof str !== "string") return "Some errors occurred, please try again";
+
+  const reg =
+    /^(?<prefix>.*)(?<link>https?\:\/\/.+\.\w{2,3}(\:\d{2,5})?)(?<suffix>.*)$/gis;
+  const res = reg.exec(str);
+  if (!res) return str;
+  if (!res.groups) return str;
+  const { groups } = res;
+  const href = groups.link;
+  if (!href) return str;
+  const a = "xxxxx";
+  groups.link = a;
+  return Object.values(groups);
+}
+
+console.log(handleLink(+str));
