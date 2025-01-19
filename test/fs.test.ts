e85cf241e38cb4e8f7f9fb421785c3a4e9daab59
yanglee2421 <yanglee2421@gmail.com>
2025-01-19 21:41:37 +0800

    chore: delete unused file

--- test/fs.test.ts
+++ test/fs.test.ts
@@ -2,31 +2,10 @@ import fs from "node:fs/promises";
 import path from "node:path";
 const filePath = path.resolve(__dirname, "../README.md");
 const neoPath = path.resolve(__dirname, "./l.txt");
-// 追写
-fs.appendFile(filePath, "")
-  .then((res) => {})
-  .catch((err) => {});
-// 读
-fs.readFile(filePath)
-  .then((file) => {})
-  .catch((err) => {});
-// 删文件or文件夹
-fs.rm(filePath)
-  .then((res) => {})
-  .catch((err) => {});
-// 复制
-fs.copyFile(filePath, neoPath)
-  .then((res) => {})
-  .catch((err) => {});
-// 重命名
-fs.rename(filePath, neoPath)
-  .then((res) => {})
-  .catch((err) => {});
-// 创建folder
-fs.mkdir(filePath)
-  .then((res) => {})
-  .catch((err) => {});
-// 移除folder
-fs.rmdir(filePath)
-  .then((res) => {})
-  .catch((err) => {});
+
+fs.appendFile(filePath, "");
+fs.readFile(filePath);
+fs.rm(filePath);
+fs.copyFile(filePath, neoPath);
+fs.rename(filePath, neoPath);
+fs.mkdir(filePath);
--- test/promise.test.js
+++
@@ -1,7 +0,0 @@
-const p = Promise.resolve("success");
-p.then((res) => {
-  console.log("then1");
-  p.then((res) => {
-    console.log("then2");
-  });
-});
